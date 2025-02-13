ded1d76bef05238ccd01d07983a1800746b9d930
Mohammed Tayeb <142062420+Tayebbb@users.noreply.github.com>
2025-02-13 16:26:44 +0600

    Update splashanimation.dart

--- lib/splashanimation.dart
+++ lib/splashanimation.dart
@@ -2,30 +2,30 @@ import 'package:austhir/login.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'dart:async';
-
+ 
 class SplashAnimation extends StatelessWidget {
   const SplashAnimation({super.key});
-
+ 
   @override
   Widget build(BuildContext context) {
-    // Navigate after 3 seconds
     Future.delayed(const Duration(seconds: 3), () {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => const SignIn()),
       );
     });
-
+ 
     return Scaffold(
       backgroundColor: Colors.white,
       body: SizedBox(
-        width: double.infinity, // Takes full screen width
-        height: double.infinity, // Takes full screen height
+        width: double.infinity, 
+        height: double.infinity, 
         child: Lottie.asset(
-          'assets/splash_animation.json', // Path to your Lottie JSON
-          fit: BoxFit.contain, // Keeps the main content centered
+          'assets/splash_animation.json', 
+          fit: BoxFit.contain, 
         ),
       ),
     );
   }
 }
+ 
