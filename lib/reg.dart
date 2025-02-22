7bb6189ea559fd1ed23eb8224ec77ca91c13e4c6
saidulislamshehab <saidulislamshehab@gmail.com>
2025-02-23 02:13:44 +0600

    reg theme changed

--- lib/reg.dart
+++ lib/reg.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
 // ignore: camel_case_types
-class reg extends StatelessWidget {
+class reg extends StatefulWidget {
   const reg({super.key});
+
+  @override
+  _regState createState() => _regState();
+}
+
+class _regState extends State<reg> {
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black12,
+      backgroundColor: Colors.white,  // Set background color to white
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Center(
@@ -18,7 +25,7 @@ class reg extends StatelessWidget {
                 Align(
                   alignment: Alignment.topLeft,
                   child: IconButton(
-                    icon: const Icon(Icons.arrow_back, color: Colors.white),
+                    icon: const Icon(Icons.arrow_back, color: Colors.indigo), // Indigo color for icon
                     onPressed: () {
                       Navigator.pop(context);
                     },
@@ -27,7 +34,7 @@ class reg extends StatelessWidget {
                 const Text(
                   "Let's create your account",
                   style: TextStyle(
-                    color: Colors.white,
+                    color: Colors.indigo, // Indigo color for text
                     fontSize: 24,
                     fontWeight: FontWeight.bold,
                   ),
@@ -43,27 +50,36 @@ class reg extends StatelessWidget {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Checkbox(value: false, onChanged: (value) {}),
+                    Checkbox(
+                      value: isChecked,
+                      onChanged: (value) {
+                        setState(() {
+                          isChecked = value!;
+                        });
+                      },
+                      checkColor: Colors.white,  // White color for checkmark
+                      fillColor: MaterialStateProperty.all(Colors.indigo), // Indigo checkbox fill color
+                    ),
                     const Text(
                       'I agree to ',
-                      style: TextStyle(color: Colors.white),
+                      style: TextStyle(color: Colors.black), // Black color for text
                     ),
                     GestureDetector(
                       onTap: () {},
                       child: const Text(
                         'Privacy Policy',
-                        style: TextStyle(color: Colors.blue),
+                        style: TextStyle(color: Colors.indigo), // Indigo color for links
                       ),
                     ),
                     const Text(
                       ' and ',
-                      style: TextStyle(color: Colors.white),
+                      style: TextStyle(color: Colors.black), // Black color for text
                     ),
                     GestureDetector(
                       onTap: () {},
                       child: const Text(
                         'Terms of use',
-                        style: TextStyle(color: Colors.blue),
+                        style: TextStyle(color: Colors.indigo), // Indigo color for links
                       ),
                     ),
                   ],
@@ -72,20 +88,20 @@ class reg extends StatelessWidget {
                 ElevatedButton(
                   onPressed: () {},
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white,
+                    backgroundColor: Colors.indigo, // Indigo background for the button
                     padding: const EdgeInsets.symmetric(
                         horizontal: 32, vertical: 16),
                     textStyle: const TextStyle(fontSize: 16),
                   ),
                   child: const Text(
                     'Create Account',
-                    style: TextStyle(color: Colors.black),
+                    style: TextStyle(color: Colors.white), // White text for the button
                   ),
                 ),
                 const SizedBox(height: 16),
                 const Text(
                   'Or Sign Up With',
-                  style: TextStyle(color: Colors.white54),
+                  style: TextStyle(color: Colors.black54), // Slightly dark text
                 ),
                 const SizedBox(height: 16),
                 Row(
@@ -104,7 +120,7 @@ class reg extends StatelessWidget {
                     const SizedBox(width: 16),
                     InkWell(
                       onTap: () {
-                        print('google');
+                        print('facebook');
                       },
                       child: SizedBox(
                         width: 35,
@@ -128,13 +144,13 @@ class reg extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 8.0),
       child: TextField(
         obscureText: isPassword,
-        style: const TextStyle(color: Colors.white),
+        style: const TextStyle(color: Colors.black), // Black text for text fields
         decoration: InputDecoration(
-          prefixIcon: Icon(icon, color: Colors.white),
+          prefixIcon: Icon(icon, color: Colors.indigo), // Indigo icon color
           hintText: hintText,
-          hintStyle: const TextStyle(color: Colors.white54),
+          hintStyle: const TextStyle(color: Colors.indigo), // Indigo hint text color
           filled: true,
-          fillColor: Colors.grey[900],
+          fillColor: Colors.indigo[50], // Light indigo fill color for the text field
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(16.0),
             borderSide: BorderSide.none,
