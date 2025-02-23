e638072a0076227439745c0ca3d2439444691790
saidulislamshehab <saidulislamshehab@gmail.com>
2025-02-24 02:52:52 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'splash_screen.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: 'AIzaSyCaQyih0dUqU276oj6amrVpIpxzNnKm_vQ',
-      appId: '1:57325847678:android:b048a06c0fb8500a4b042f',
-      messagingSenderId: '57325847678',
-      projectId: 'austmaster-c4b72',
-      databaseURL: 'https://austmaster-c4b72-default-rtdb.firebaseio.com/',
-    ),
-  );
-
+void main() {
   runApp(const MyApp());
 }
 
